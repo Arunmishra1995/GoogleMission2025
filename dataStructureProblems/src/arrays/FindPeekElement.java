9307f2003610a31444b444468ffa8f99ebb74c7c
arunmishra <arunmishra7566@gmail.com>
2024-09-10 01:45:45 +0530

    Added main method to start the code

--- dataStructureProblems/src/arrays/FindPeekElement.java
+++ dataStructureProblems/src/arrays/FindPeekElement.java
@@ -1,4 +1,8 @@
 package arrays;
 
 public class FindPeekElement {
+
+    public static void main(String[] args){
+        System.out.println("Welcome to the Google world***************2024 Sept");
+    }
 }
